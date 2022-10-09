1312d69e1ca1e8b302cc6002a6f440659cfad8d7
maria <ma.usachova@gmail.com>
2022-10-10 01:38:11 +0300

    search room

--- hotel_app/hotel_app/booking_app/views.py
+++ hotel_app/hotel_app/booking_app/views.py
@@ -1,11 +1,18 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import CreateView
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Room, Booking
 from .permissions import IsOwnerOrAdminOrReadOnly, IsAdminOrReadOnly
 from .serializers import RoomSerializer, BookingSerializer
@@ -34,9 +41,19 @@ class RedistrUserView(CreateView):
 class LogoutUserView(LogoutView):
     next_page = '/'
 class BookingAPIView(generics.ListCreateAPIView):
-    queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user.id)
+
+    def post(self, request, *args, **kwargs):
+        if Booking.objects.filter(room=request.POST.get('room')).filter(Q(date_start__lte=request.POST.get('date_finish')) |
+                                                            Q(date_finish__gte=request.POST.get('date_start'))):
+            return Response({'detail': "нельзя забронировать комнату на эти даты"})
+        if request.POST.get('date_finish') < request.POST.get('date_start'):
+            return Response({'detail': "дата финиша раньше даты старта"})
+        return self.create(request, *args, **kwargs)
 
 class BookingAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Booking.objects.all()
@@ -48,11 +65,27 @@ class BookingAPIDestroy(generics.RetrieveDestroyAPIView):
     serializer_class = BookingSerializer
     permission_classes = (IsOwnerOrAdminOrReadOnly, )
 
-# class BookingAPIFilters():
-
 class RoomAPIView(generics.ListCreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
-    filter_backends = (DjangoFilterBackend, )
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_class = RoomFilter
+    ordering_fields = ['cost', 'beds_count']
     permission_classes = (IsAdminOrReadOnly, )
+
+class RoomAPISearch(APIView):
+    serializer_class = RoomSerializer
+    queryset = Room.objects.all()
+
+    def get(self, request):
+        date_start = datetime.strptime(self.request.GET.get('date_start'), '%Y-%m-%d')
+        date_finish = datetime.strptime(self.request.GET.get('date_finish'), '%Y-%m-%d')
+        rooms = Room.objects.all()
+        queryset = []
+        if date_start and date_finish:
+            for room in rooms:
+                if not room.booking_set.filter(Q(date_start__lte=date_start) | Q(date_finish__gte=date_finish)):
+                    queryset.append(room.number)
+        else:
+            queryset = [room.number for room in rooms]
+        return Response(queryset)
--- hotel_app/hotel_app/hotel_app/urls.py
+++ hotel_app/hotel_app/hotel_app/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('api/bookings', BookingAPIView.as_view()),
     path('api/booking/<int:pk>/', BookingAPIUpdate.as_view()),
     path('api/delete_booking/<int:pk>/', BookingAPIDestroy.as_view()),
+    path('api/search_room/', RoomAPISearch.as_view())
 ]
